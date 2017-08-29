1f13a32e9d9f62b0e76e7dd5a569d689781193b8
zuokylin <zuokylin@gmail.com>
2017-08-29 15:07:00 +0800

    基于gulp/pug/less 的静态网页开发模版

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+const path = require("path");
+const util = require("util");
+const gulp = require("gulp");
+const pug = require("gulp-pug");
+const stripDebug = require("gulp-strip-debug");
+const gulpLess = require("gulp-less");
+const uglify = require("gulp-uglify");
+const minify = require("gulp-minify");
+const browserSync = require("browser-sync");
+const del = require("del")
+
+const CSS_PATH = path.join(__dirname, "dist", "css")
+const JS_PATH = path.join(__dirname, "dist", "js")
+
+gulp.task("pug", () => {
+  return gulp
+    .src(["src/views/*.pug", "!src/views/layout.pug"])
+    .pipe(pug())
+    .pipe(gulp.dest(__dirname))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task("less", () => {
+  util.log("====================>> less start");
+  return gulp
+    .src("src/less/**/*.less")
+    .pipe(gulpLess())
+    .pipe(gulp.dest(CSS_PATH))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task("jsProd", () => {
+  console.log('=== jsProd')
+  return gulp
+    .src("src/js/**/*")
+    .pipe(uglify())
+    .pipe(minify())
+    .pipe(gulp.dest(JS_PATH))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task("browserSync", function(){
+  browserSync({
+    server:{
+      baseDir: "./"
+    }
+  })
+});
+
+gulp.task("watch", ["browserSync"], function () {
+  gulp.watch(["src/**/*.less", "src/**/*.js"], ["less", "jsProd"]);
+  gulp.watch(["src/**/*.pug"], ["pug"]);
+});
+
+// build
+gulp.task("build", ["pug", "less", "jsProd"]);
+
+// dev
+gulp.task("default", ["watch", "build"]);
+
+// clean
+gulp.task("clean", function(callback) {
+  del(["dist/js/*", "dist/css/*"], callback);
+});
---
+++ readme.md
@@ -0,0 +1,26 @@
+[TOC]
+
+# 项目说明
+
+基于pug/less/gulp/swiper/bootstrap的静态网页开发模版
+
+> 开发环境
+
+```
+node 8.0.0
+npm  5.0.0
+```
+
+> 开发
+
+```
+npm install
+npm run dev
+```
+
+> 编译
+
+```
+npm install
+npm run build
+```
